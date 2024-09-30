6104132ea8e7a8a62367ce21cda9980b36f8a187
Roben Delos Reyes <rdelosreyes@2111L-211122-M.local>
2024-09-30 13:40:07 +1000

    Update dataset path

--- Benchmark_scripts/dataset_path.py
+++ Benchmark_scripts/dataset_path.py
@@ -4,8 +4,9 @@
 #mimic_iv_path = '/dataset/mimiciv/1.0/'
 #output_path = '/home/jun/data/MIMIC/processed/'
 
-mimic_iv_path = '/Users/jin/Desktop/work_local/Temporal_AutoScore/Data/mimic-iv-1.0'
+mimic_iv_path = '/Users/rdelosreyes/Documents/physionet.org/files/mimiciv/2.2'
+mimic_iv_ed_path = '/Users/rdelosreyes/Documents/physionet.org/files/mimic-iv-ed/2.2'
 #mimic_iv_path = '/Users/siqili/Desktop/National University of Singapore/Liu Nan - WP_MIMIC_Benchmark/OriginalData/data'
 
-output_path = '/Users/jin/Desktop/work_local/mimic4/Output_270922'
+output_path = '/Users/rdelosreyes/Documents/Code/ai_harms_in_the_ed/mimic4ed-benchmark/data'
 #output_path = '/Users/siqili/Desktop/National University of Singapore/Liu Nan - WP_MIMIC_Benchmark'
